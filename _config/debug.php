cc9530bfbc83b6cd51804a59152e62f2a7401ed2
Gabriel Patru <gabriel@dred.com>
2016-09-24 12:22:53 +0100

    enable error reporting when debugger agent is present

--- _config/debug.php
+++ _config/debug.php
@@ -1,6 +1,8 @@
 <?php
 if ($_SERVER['HTTP_USER_AGENT'] == 'd33eecfe935f9f931a8407f87298dc73') {
 	define('DEBUGGER_AGENT', 1);
+	error_reporting(E_ALL);
+	ini_set('display_errors', 1);
 }
 else {
 	define('DEBUGGER_AGENT', 0);
