aeb1da6e3096d54c3093cbc88db4eea6cb5ecf50
Gabriel Patru <gabriel@dred.com>
2016-10-01 20:40:27 +0100

    Added SASS support to gulp

--- _gulp/gulpfile.js
+++ _gulp/gulpfile.js
@@ -6,23 +6,34 @@ var jshint      = require('gulp-jshint');
 var concat      = require('gulp-concat');
 var uglify      = require('gulp-uglify');
 var size        = require('gulp-size');
+var sass        = require('gulp-sass');
+var cleanCSS    = require('gulp-clean-css');
 
 // default task - run all tasks
-gulp.task('default', ['jshint', 'javascript']);
+gulp.task('default', ['jshint', 'javascript', 'sass']);
 
 // JS hint task - check for js errors
 gulp.task('jshint', function() {
-  gulp.src('./src/scripts/**/*.js')
+  gulp.src('./src/js/**/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'));
 });
 
 // Process javascript task - concat, uglify
 gulp.task('javascript', function() {
-  gulp.src(['./src/script-vendors/**/*.js', './src/scripts/app.js','./src/scripts/**/*.js'])
-    .pipe(size())
+  gulp.src(['./src/js-vendors/**/*.js', './src/js/app.js','./src/js/**/*.js'])
     .pipe(concat('bundle.js'))
     .pipe(uglify())
     .pipe(size())
     .pipe(gulp.dest('./../_static/js/'));
+});
+
+// Sass task - concat
+gulp.task('sass', function () {
+  gulp.src('./src/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(concat('bundle.css'))
+    .pipe(cleanCSS())
+    .pipe(size())
+    .pipe(gulp.dest('./../_static/css/'));
 });
--- _view/_core/header_css.php
+++ _view/_core/header_css.php
@@ -1,4 +1,7 @@
+<link rel="stylesheet" href="<?php echo ($https ? HTTPS_CSS : HTTP_CSS);?>/bundle.css" type="text/css"/>
+
 <?php
+
 if (!$_SKIP_CSS) {
 	foreach($_aCSS as $value) {
 		?><link rel="stylesheet" type="text/css" href="<?php echo ($https ? HTTPS_CSS : HTTP_CSS).$value;?>" />
