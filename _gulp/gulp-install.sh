fce27b73b55299566cbbb84776bc96403578af0a
Gabi Patru <gabipatru@gmail.com>
2017-04-08 23:02:11 +0100

    Some new gulp files

---
+++ _gulp/gulp-install.sh
@@ -0,0 +1,10 @@
+sudo npm install --global gulp
+sudo npm install --save-dev gulp
+sudo npm install --save-dev gulp-util
+sudo npm install --save-dev jshint
+sudo npm install --save-dev gulp-jshint jshint-stylish
+sudo npm install --save-dev gulp-concat
+sudo npm install --save-dev gulp-uglify
+sudo npm install --save-dev gulp-size
+sudo npm install --save-dev gulp-sass
+sudo npm install --save-dev gulp-clean-css
