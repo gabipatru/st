beed22ceda2051bb8bf7736a3e26d5b87d007e3e
Gabi Patru <gabipatru@gmail.com>
2017-04-08 23:44:17 +0100

    Update config table

--- _migrations/config/001.php
+++ _migrations/config/001.php
@@ -3,7 +3,7 @@ $migrationSql = array();
 
 $migrationSql[] ="
     CREATE TABLE config(
-      config_id INT(10) NOT NULL AUTO_INCREMENT,
+      config_id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
       path VARCHAR(255) NOT NULL,
       value TEXT,
       PRIMARY KEY (config_id)
