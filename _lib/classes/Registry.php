c3ee05767e435191fece412ac56dce4848565491
Gabi Patru <gabipatru@gmail.com>
2017-04-14 22:15:10 +0100

    Added class for php memory storage

---
+++ _lib/classes/Registry.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ * This class is a singleton which caches all sort of stuff useful in the application
+ */
+
+class Registry {
+    private static $instance = null;
+    private $data = array();
+    
+    protected function __construct() {
+    
+    }
+    
+    private function __clone() {
+    
+    }
+    
+    private function __wakeup() {
+    
+    }
+    
+    public static function getInstance() {
+        if (!static::$instance) {
+            static::$instance = new static;
+        }
+    
+        return static::$instance;
+    }
+    
+    public function set($key, $data, $overwrite = false) {
+        if (isset($this->data[$key]) && $overwrite == false) {
+            return false;
+        }
+        $this->data[$key] = $data;
+    }
+    
+    public function get($key) {
+        if (isset($this->data[$key])) {
+            return $this->data[$key];
+        }
+    }
+    
+    public function clear($key) {
+        if (isset($this->data[$key])) {
+            unset($this->data[$key]);
+        }
+    }
+}
