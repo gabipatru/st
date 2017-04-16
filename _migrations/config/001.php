125984a0007bca3451bebf8d2d32e61243eef871
Gabi Patru <gabipatru@gmail.com>
2017-04-16 01:55:04 +0100

    Updated Config migrations

--- _migrations/config/001.php
+++ _migrations/config/001.php
@@ -6,7 +6,8 @@ $migrationSql[] ="
       config_id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
       path VARCHAR(255) NOT NULL,
       value TEXT,
-      PRIMARY KEY (config_id)
+      PRIMARY KEY (config_id),
+      UNIQUE INDEX (`path`)
     )
     COLLATE='latin1_general_ci'
     ENGINE=InnoDB;
