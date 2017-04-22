8c39b72c3233fa8432de058d7008361877673ac6
Gabi Patru <gabipatru@gmail.com>
2017-04-22 22:38:39 +0100

    Allow adding of users

--- _controller/user.php
+++ _controller/user.php
@@ -17,6 +17,86 @@ class controller_user {
     }
     
     function newuser() {
+        $FV = new FormValidation(array(
+            'rules' => array(
+                'email' => array(
+                    'required' => true, 
+                    'email' => true
+                ),
+                'username' => array(
+                    'required',
+                    'minlength' => 2
+                ),
+                'password' => array(
+                    'required' => true,
+                    'minlength' => 2
+                ),
+                'password2' => array(
+                    'required' => true,
+                    'equalTo' => 'password'
+                ),
+                'first_name' => 'required',
+                'last_name' => 'required'
+            ),
+            'messages' => array(
+                'email'         => _('Please fill a valid email address'),
+                'username'      => _('Please choose a username, at least 2 characters long'),
+                'password'      => _('Password is not strong enough'),
+                'password2'     => _('Passwords must be identical'),
+                'first_name'    => _('Please fill in your first name'),
+                'last_name'     => _('Please fill in your last name')
+            ),
+        ));
         
+        $validateResult = $FV->validate();
+        if (isPOST()) {
+            try {
+                if (!$validateResult) {
+                    throw new Exception(_('Please fill all the required fields'));
+                }
+                
+                $oItem = new SetterGetter();
+                $oItem->setEmail(filter_post('email', 'email'));
+                $oItem->setUsername(filter_post('username', 'clean_html'));
+                $oItem->setPassword(filter_post('password', 'string'));
+                $oItem->setFirstName(filter_post('first_name', 'clean_html'));
+                $oItem->setLastName(filter_post('last_name', 'clean_html'));
+                
+                $oUser = new User();
+                
+                // check if another user with that username exists
+                $filters = array('username' => $oItem->getUsername());
+                $options = array();
+                $Collection = $oUser->Get($filters, $options);
+                if (count($Collection)) {
+                    throw new Exception(_('This username is already taken. Please choose another'));
+                }
+                
+                // check if another user with that email exists
+                $filters = array('email' => $oItem->getEmail());
+                $options = array();
+                $Collection = $oUser->Get($filters, $options);
+                if (count($Collection)) {
+                    throw new Exception(_('A user with that email already exists. Please use another email'));
+                }
+                
+                // add the user
+                $r = $oUser->Add($oItem);
+                if (!$r) {
+                    throw new Exception('Error adding user to the database. Please try again later');
+                }
+                
+                // @TODO: add confirmation if user is not active
+                
+                // @TODO: send welcome email
+                
+                message_set(_('User added to the database'));
+            }
+            catch (Exception $e) {
+                message_set_error($e->getMessage());
+            }
+        }
+        
+        mvc::assign('FV', $FV);
     }
 }
--- _view/user/newuser.php
+++ _view/user/newuser.php
@@ -2,27 +2,33 @@
 <form id="newuser" action="<?php echo MVC_ACTION_URL?>" method="post">
 	<p>
 		<span><?php echo _('Email address')?>:</span>
-		<input type="text" name="email">
+		<input type="text" name="email" value="<?php echo $FV->email?>">
+		<label id="email-error" class="error" for="email"><?php echo $FV->email_error?></label>
 	</p>
 	<p>
 		<span><?php echo _('Username')?>:</span>
-		<input type="text" name="username">
+		<input type="text" name="username" value="<?php echo $FV->username?>">
+		<label id="username-error" class="error" for="username"><?php echo $FV->username_error?></label>
 	</p>
 	<p>
 		<span><?php echo _('Password')?>:</span>
 		<input type="password" name="password">
+		<label id="password-error" class="error" for="password"><?php echo $FV->password_error?></label>
 	</p>
 	<p>
-		<span><?php echo _('Repeat Password')?>:</span>
+		<span><?php echo _('Retype Password')?>:</span>
 		<input type="password" name="password2">
+		<label id="password2-error" class="error" for="password2"><?php echo $FV->password2_error?></label>
 	</p>
 	<p>
 		<span><?php echo _('First Name')?>:</span>
-		<input type="text" name="first_name">
+		<input type="text" name="first_name" value="<?php echo $FV->first_name?>">
+		<label id="first_name-error" class="error" for="first_name"><?php echo $FV->first_name_error?></label>
 	</p>
 	<p>
 		<span><?php echo _('Last Name')?>:</span>
-		<input type="text" name="last_name">
+		<input type="text" name="last_name" value="<?php echo $FV->last_name?>">
+		<label id="last_name-error" class="error" for="last_name"><?php echo $FV->last_name_error?></label>
 	</p>
 	
 	<input type="submit" value="<?php echo _('Create')?>">
