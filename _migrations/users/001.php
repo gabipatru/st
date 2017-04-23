0575425458390b3ccfc3f358dfe3b0e1f53b7139
Gabi Patru <gabipatru@gmail.com>
2017-04-23 01:59:21 +0100

    Update users migration

--- _migrations/users/001.php
+++ _migrations/users/001.php
@@ -5,10 +5,11 @@ CREATE TABLE `user`(
     `user_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
     `email` VARCHAR(255) NOT NULL,
     `username` VARCHAR(255) NOT NULL,
-    `password` CHAR(60) NOT NULL,
+    `password` CHAR(40) NOT NULL,
     `first_name` VARCHAR(255) NOT NULL,
     `last_name` VARCHAR(255) NOT NULL,
     `status` ENUM('active', 'new', 'banned') NOT NULL,
+    `is_admin` SMALLINT NOT NULL DEFAULT 0,
     `last_login` TIMESTAMP NULL DEFAULT NULL,
     `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`user_id`),
@@ -18,4 +19,10 @@ CREATE TABLE `user`(
 )
 COLLATE='latin1_general_ci'
 ENGINE=InnoDB;
+";
+
+// add admin user
+$migrationSql[] = "
+INSERT INTO `user` (email, username, password, first_name, last_name, status, is_admin)
+VALUES('gabipatru@gmail.com', 'admin', '632ccb20c31207fb22bf34a5c32fc4e24f6779aa', 'Gabi', 'Patru', 'active', 1);
 ";
