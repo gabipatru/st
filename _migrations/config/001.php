c4fa4261c0243a34ba74c08da2aa51356ae64e2a
Gabi Patru <gabipatru@gmail.com>
2017-04-26 21:24:22 +0100

    Implement support for more config types in DB

--- _migrations/config/001.php
+++ _migrations/config/001.php
@@ -6,6 +6,7 @@ $migrationSql[] ="
       config_id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
       path VARCHAR(255) NOT NULL,
       value TEXT,
+      type ENUM('text', 'textarea', 'yesno'),
       PRIMARY KEY (config_id),
       UNIQUE INDEX (`path`)
     )
