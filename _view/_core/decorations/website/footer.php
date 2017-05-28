d056d31ceaa80ae86995099c19ec2784adce9a32
Gabi Patru <gabipatru@gmail.com>
2017-05-28 17:21:40 +0100

    Added top header, bottom footer

--- _view/_core/decorations/website/footer.php
+++ _view/_core/decorations/website/footer.php
@@ -1,6 +1,16 @@
 
+</header>
 
-</div> <!-- page-wrapper -->
+<footer id="footer">
+	<div class="footer-bottom">
+		<div class="container">
+			<div class="row">
+				<p class="pull-left">Copyright © 2013 E-SHOPPER Inc. All rights reserved.</p>
+				<p class="pull-right">Designed by <span><a target="_blank" href="http://www.themeum.com">Themeum</a></span></p>
+			</div>
+		</div>
+	</div>
+</footer>
 
 <?php require_once(VIEW_DIR.'/_core/header_js.php');?>
 
