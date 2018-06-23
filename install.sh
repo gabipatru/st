23b1ef6f039f5b5a2f7e841d0e611209d1d63020
Gabriel Patru <gabipatru@gmail.com>
2018-06-23 13:52:59 +0000

    Added installer for npm, gulp and phpunit

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+path=/var/www/st
+
+# Install npm and gulp
+cd "$path/_gulp"
+npm install
+npm install -g gulp
+
+# Install phpunit
+cd "$path/_test"
+wget -O phpunit https://phar.phpunit.de/phpunit-7.phar
+chmod +x phpunit
+
+# check phpunit
+./phpunit --version 
