e86ea2df9a1185530ec9a22669ac5ec9a8dc8568
Gabriel Patru <gabipatru@gmail.com>
2018-06-24 11:42:48 +0000

    Addded unit test for Translations

---
+++ _test/_lib/Translations.test.php
@@ -0,0 +1,92 @@
+<?php
+namespace Test;
+
+use PHPUnit\Framework\TestCase;
+
+require_once(__DIR__ .'/../AbstractTest.php');
+
+class Translations extends AbstractTest {
+    
+    /**
+     * Test if the Translations class is a singleton
+     * @group fast
+     */
+    public function testSingleton() {
+        $tr1 = \Translations::getSingleton();
+        $tr1->setModule('common', false);
+        
+        $tr2 = \Translations::getSingleton();
+        $tr2->setModule('website', false);
+        
+        $this->assertEquals($tr1->getModule(), $tr2->getModule());
+        
+        $tr1->setLanguage('ro_RO');
+        $tr2->setLanguage('en_EN');
+        
+        $this->assertEquals($tr1->getLanguage(), $tr2->getLanguage());
+    }
+    
+    /**
+     * Test if a langiage exists or not
+     * @group fast
+     */
+    public function testLanguageExists() {
+        $tr = \Translations::getSingleton();
+        
+        $this->assertTrue($tr->checkIfLanguageExists('en_EN'));
+        $this->assertTrue($tr->checkIfLanguageExists('ro_RO'));
+        $this->assertFalse($tr->checkIfLanguageExists('pl_PL'));
+        $this->assertFalse($tr->checkIfLanguageExists('bla'));
+    }
+    
+    /**
+     * Test if the number if translated strings for different languages
+     * are the same
+     * @group fast
+     */
+    public function testTranslationIntegrity() {
+        $tr = \Translations::getSingleton();
+        
+        foreach (\Translations::MODULES as $module) {
+            $tr->setLanguage('en_EN');
+            $tr->setModule($module);
+            $nrStringsEn = $tr->getAllKeys();
+            
+            $tr->resetTranslations();
+            
+            $tr->setLanguage('ro_RO');
+            $tr->setModule($module);
+            $nrStringsRo = $tr->getAllKeys();
+            
+            $tr->resetTranslations();
+            
+            $this->assertEquals($nrStringsEn, $nrStringsRo);
+        }
+    }
+    
+    /**
+     * Test some translations in action
+     * @group fast
+     */
+    public function testTranslations() {
+        // set up language and module
+        $tr = \Translations::getSingleton();
+        $tr->setLanguage('en_EN');
+        $tr->setModule('common');
+        
+        // asserts
+        $this->assertEquals('Account', $tr->__('Account'));
+        $this->assertEquals('Home', $tr->__('Home'));
+        
+        $tr->resetTranslations();
+        
+        // set up language and module
+        $tr = \Translations::getSingleton();
+        $tr->setLanguage('ro_RO');
+        $tr->setModule('common');
+        
+        // asserts
+        $this->assertEquals('Cont', $tr->__('Account'));
+        $this->assertEquals('Acasa', $tr->__('Home'));
+    }
+}
