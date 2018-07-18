2c9b4b0167044ce2537a1de1457a5d6f58920849
Gabriel Patru <gabipatru@gmail.com>
2018-07-18 17:56:32 +0000

    Changed admin layout

--- _view/admin/config/list_items.php
+++ _view/admin/config/list_items.php
@@ -1,6 +1,45 @@
 <!-- Main -->
 <div id="main">
 	<div class="cl">&nbsp;</div>
+ 
+    <!-- Sidebar -->
+    <div id="sidebar">
+    
+    <!-- Box -->
+    <div class="box">
+   
+        <!-- Box Head -->
+        <div class="box-head">
+            <h2><?php echo __('Management')?></h2>
+        </div>
+        <!-- End Box Head-->
+     
+        <div class="box-content">
+           <a href="<?php echo href_admin('config/add')?>" class="add-button"><span><?php echo __('Add new Config')?></span></a>
+           <div class="cl">&nbsp;</div>
+        </div>
+    </div>
+ 
+    <!-- Box -->
+    <div class="box">
+   
+        <!-- Box Head -->
+        <div class="box-head">
+            <h2><?php echo __('Config')?></h2>
+        </div>
+        <!-- End Box Head-->
+     
+        <div class="box-content">
+        <?php foreach ($aConfig as $config_name => $ConfigItem):?>
+            <a href="<?php echo MVC_ACTION_URL?>?name=<?php echo $config_name?>" class="box-link"><?php echo $config_name;?></a>
+        <?php endforeach;?>
+            <div class="cl">&nbsp;</div>
+        </div>
+    </div>
+    <!-- End Box -->
+    
+    </div>
+    <!-- End Sidebar -->
 	
 	<!-- Content -->
 	<div id="content">
@@ -59,43 +98,7 @@
 	</div>
 	<!-- End Content -->
 			
-	<!-- Sidebar -->
-	<div id="sidebar">
-				
-	<!-- Box -->
-	<div class="box">
-			
-		<!-- Box Head -->
-		<div class="box-head">
-			<h2><?php echo __('Management')?></h2>
-		</div>
-		<!-- End Box Head-->
-					
-		<div class="box-content">
-			<a href="<?php echo href_admin('config/add')?>" class="add-button"><span><?php echo __('Add new Config')?></span></a>
-		<div class="cl">&nbsp;</div>
-		</div>
-	</div>
 	
-	<!-- Box -->
-	<div class="box">
-			
-		<!-- Box Head -->
-		<div class="box-head">
-			<h2><?php echo __('Config')?></h2>
-		</div>
-		<!-- End Box Head-->
-					
-		<div class="box-content">
-			<?php foreach ($aConfig as $configName => $ConfigItem):?>
-				<a href="<?php echo MVC_ACTION_URL?>?name=<?php echo $configName?>" class="box-link"><?php echo $configName;?></a>
-			<?php endforeach;?>
-		<div class="cl">&nbsp;</div>
-		</div>
-	</div>
-	<!-- End Box -->
-	</div>
-	<!-- End Sidebar -->
 			
 	<div class="cl">&nbsp;</div>			
 </div>
