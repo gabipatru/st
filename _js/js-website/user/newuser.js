9da67d766cb35fef1d22724d259e22949a3b221b
Gabriel Patru <gabipatru@gmail.com>
2018-07-20 19:12:17 +0000

    Display password strength when creating new user

--- _js/js-website/user/newuser.js
+++ _js/js-website/user/newuser.js
@@ -1,5 +1,14 @@
-addAction('controller_user', 'newuser', {
+addAction( 'controller_user', 'newuser', {
     run: function() {
-        $("#newUser").validate(aFormValidate);
+        $( "#newUser" ).validate( aFormValidate );
+        
+        // password strength meter
+        $( "#password" ).on('input', function() {
+            // get the strength
+            Strength = zxcvbn( $( this ).val() );
+            
+            // update the meter and the text
+            $( "#password-strength-meter" ).val( Strength.score );
+        })
     },
 });
--- _view/user/newuser.php
+++ _view/user/newuser.php
@@ -1,57 +1,59 @@
 <div id="form-page">
-	<div class="bg">
-		<div class="row">
-			<div class="col-sm-12">
-				<h2 class="title text-center"><?php echo __('Create account')?></h2>
-			</div>			 		
-		</div>
+    <div class="bg">
+        <div class="row">
+            <div class="col-sm-12">
+                <h2 class="title text-center"><?php echo __('Create account')?></h2>
+            </div>                     
+        </div>
 <?php if (!$userAdded):?>
-		<div class="row">
-			<div class="col-sm-4">
-				<div class="contact-form">
-				
+        <div class="row">
+            <div class="col-sm-4">
+                <div class="contact-form">
+                
 <form id="newUser" class="contact-form row" action="<?php echo MVC_ACTION_URL?>" method="post">
-	<div class="form-group col-md-12">
-		<span><?php echo __('Email address')?>:</span>
-		<input type="text" class="form-control" name="email" placeholder="<?php echo __('Email address')?>" value="<?php echo $FV->email?>">
-		<label id="email-error" class="error" for="email"><?php echo $FV->email_error?></label>
-	</div>
-	<div class="form-group col-md-12">
-		<span><?php echo __('Username')?>:</span>
-		<input type="text" class="form-control" name="username" placeholder="<?php echo __('Username')?>" value="<?php echo $FV->username?>">
-		<label id="username-error" class="error" for="username"><?php echo $FV->username_error?></label>
-	</div>
-	<div class="form-group col-md-12">
-		<span><?php echo __('Password')?>:</span>
-		<input type="password" class="form-control" name="password" id="password">
-		<label id="password-error" class="error" for="password"><?php echo $FV->password_error?></label>
-	</div>
-	<div class="form-group col-md-12">
-		<span><?php echo __('Retype Password')?>:</span>
-		<input type="password" class="form-control" name="password2">
-		<label id="password2-error" class="error" for="password2"><?php echo $FV->password2_error?></label>
-	</div>
-	<div class="form-group col-md-12">
-		<span><?php echo __('First Name')?>:</span>
-		<input type="text" class="form-control" name="first_name" placeholder="<?php echo __('First Name')?>" value="<?php echo $FV->first_name?>">
-		<label id="first_name-error" class="error" for="first_name"><?php echo $FV->first_name_error?></label>
-	</div>
-	<div class="form-group col-md-12">
-		<span><?php echo __('Last Name')?>:</span>
-		<input type="text" class="form-control" name="last_name" placeholder="<?php echo __('Last Name')?>" value="<?php echo $FV->last_name?>">
-		<label id="last_name-error" class="error" for="last_name"><?php echo $FV->last_name_error?></label>
-	</div>
-	
-	<div class="form-group col-md-12">
-		<input type="submit" class="btn btn-primary pull-right" value="<?php echo __('Create User')?>">
-	</div>
-	<input type="hidden" name="token" value="<?php echo securityGetToken()?>">
+    <div class="form-group col-md-12">
+        <span><?php echo __('Email address')?>:</span>
+        <input type="text" class="form-control" name="email" placeholder="<?php echo __('Email address')?>" value="<?php echo $FV->email?>">
+        <label id="email-error" class="error" for="email"><?php echo $FV->email_error?></label>
+    </div>
+    <div class="form-group col-md-12">
+        <span><?php echo __('Username')?>:</span>
+        <input type="text" class="form-control" name="username" placeholder="<?php echo __('Username')?>" value="<?php echo $FV->username?>">
+        <label id="username-error" class="error" for="username"><?php echo $FV->username_error?></label>
+    </div>
+    <div class="form-group col-md-12">
+        <span><?php echo __('Password')?>:</span>
+        <meter id="password-strength-meter" max="4"></meter>
+        <span id="password-strength-text"></span>
+        <input type="password" class="form-control" name="password" id="password">
+        <label id="password-error" class="error" for="password"><?php echo $FV->password_error?></label>
+    </div>
+    <div class="form-group col-md-12">
+        <span><?php echo __('Retype Password')?>:</span>
+        <input type="password" class="form-control" name="password2">
+        <label id="password2-error" class="error" for="password2"><?php echo $FV->password2_error?></label>
+    </div>
+    <div class="form-group col-md-12">
+        <span><?php echo __('First Name')?>:</span>
+        <input type="text" class="form-control" name="first_name" placeholder="<?php echo __('First Name')?>" value="<?php echo $FV->first_name?>">
+        <label id="first_name-error" class="error" for="first_name"><?php echo $FV->first_name_error?></label>
+    </div>
+    <div class="form-group col-md-12">
+        <span><?php echo __('Last Name')?>:</span>
+        <input type="text" class="form-control" name="last_name" placeholder="<?php echo __('Last Name')?>" value="<?php echo $FV->last_name?>">
+        <label id="last_name-error" class="error" for="last_name"><?php echo $FV->last_name_error?></label>
+    </div>
+    
+    <div class="form-group col-md-12">
+        <input type="submit" class="btn btn-primary pull-right" value="<?php echo __('Create User')?>">
+    </div>
+    <input type="hidden" name="token" value="<?php echo securityGetToken()?>">
 </form>
 <?php echo $FV->_js_code;?>
 
-				</div>
-			</div>
-		</div> <!-- End row -->
+                </div>
+            </div>
+        </div> <!-- End row -->
 <?php endif;?>
-	</div> <!-- End bg -->
+    </div> <!-- End bg -->
 </div> <!-- End form page -->
